5ad4daef0062c862214140ec3cd2d27e09338611
gonzalo <gonzalo@ebizmarts.com>
2016-04-29 11:51:31 -0300

    Initial commit

---
+++ src/Mailchimp.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * mailchimp-lib Magento Component
+ *
+ * @category Ebizmarts
+ * @package mailchimp-lib
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 4/27/16 4:36 PM
+ * @file: Mailchimp.php
+ */
+
+require_once('Malchimp/Lists.php');
+require_once('Mailchimp/Exceptions.php');
+
+
+class Mailchimp
+{
+    public $apiKey;
+    public $ch;
+    public $root    = 'https://api.mailchimp.com/3.0';
+    public $debug   = false;
+    const POST      = 'POST';
+    const GET       = 'GET';
+
+    public function __construct($apiKey=null,$opts=array())
+    {
+        if(!$apiKey)
+        {
+            throw new Mailchimp_Error('You must provide a MailChimp API key');
+        }
+        $this->apiKey   = $apiKey;
+        $dc             = 'us1';
+        if (strstr($this->apiKey, "-")){
+            list($key, $dc) = explode("-", $this->apiKey, 2);
+            if (!$dc) {
+                $dc = "us1";
+            }
+        }
+        $this->root = str_replace('https://api', 'https://' . $dc . '.api', $this->root);
+        $this->root = rtrim($this->root, '/') . '/';
+
+        if (!isset($opts['timeout']) || !is_int($opts['timeout'])){
+            $opts['timeout'] = 600;
+        }
+        if (isset($opts['debug'])){
+            $this->debug = true;
+        }
+
+
+        $this->ch = curl_init();
+
+        if (isset($opts['CURLOPT_FOLLOWLOCATION']) && $opts['CURLOPT_FOLLOWLOCATION'] === true) {
+            curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, true);
+        }
+
+        curl_setopt($this->ch, CURLOPT_USERAGENT, 'Ebizmart-MailChimp-PHP/3.0.0');
+        curl_setopt($this->ch, CURLOPT_POST, true);
+        curl_setopt($this->ch, CURLOPT_HEADER, false);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($this->ch, CURLOPT_TIMEOUT, $opts['timeout']);
+
+        $this->lists= new Mailchimp_Lists($this);
+
+    }
+    public function call($url,$params,$method='GET')
+    {
+        $params['apikey'] = $this->apiKey;
+        $params = json_encode($params);
+
+        $ch = $this->ch;
+        curl_setopt($ch, CURLOPT_URL, $this->root . $url . '.json');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_VERBOSE, $this->debug);
+
+        $response_body = curl_exec($ch);
+
+        $info = curl_getinfo($ch);
+        if(curl_error($ch)) {
+            throw new Mailchimp_HttpError("API call to $url failed: " . curl_error($ch));
+        }
+        $result = json_decode($response_body, true);
+
+        if(floor($info['http_code'] / 100) >= 4) {
+            throw $this->castError($result);
+        }
+
+        return $result;
+    }
+    public function castError($result) {
+        if ($result['status'] !== 'error' || !$result['name']) {
+            throw new Mailchimp_Error('We received an unexpected error: ' . json_encode($result));
+        }
+
+        $class = (isset(self::$error_map[$result['name']])) ? self::$error_map[$result['name']] : 'Mailchimp_Error';
+        return new $class($result['error'], $result['code']);
+    }
+}
---
+++ src/Mailchimp/Lists.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * mailchimp-lib Magento Component
+ *
+ * @category Ebizmarts
+ * @package mailchimp-lib
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 4/27/16 5:00 PM
+ * @file: Lists.php
+ */
+
+class Mailchimp_Lists
+{
+    /*
+     * @var Mailchimp
+     */
+    protected $master;
+
+    /**
+     * @param Mailchimp $m
+     */
+    public function __contruct(Mailchimp $m)
+    {
+        $this->master = $m;
+    }
+
+    /**
+     * @param $name
+     * @param $contact
+     *          company *   (The company name for the list)
+     *          address1 *  (The street address for the list contact)
+     *          address2    (The street address for the list contact)
+     *          city *      (The city for the list contact)
+     *          state *     (The state for the list contact)
+     *          zip *       (The postal or zip code for the list contact)
+     *          country *   (A two-character ISO3166 country code. Defaults to US if invalid.)
+     * @param $permissionRemanider
+     * @param bool|false $useArchiveBar
+     * @param $campaingDefaults
+     *          fromName * (The default from name for campaigns sent to this list)
+     *          fromEmail * (The email address to send unsubscribe notifications to)
+     *          subject * (The default subject line for campaigns sent to this list)
+     *          language *(The default language for this lists’s forms)
+     * @param bool|false $notifiyOnSubscribe
+     * @param $notifyOnUnsubscribe
+     * @param $emailTypeOption
+     * @param string $visibility
+     */
+    public function add($name,$contact,$permissionRemanider,$useArchiveBar=false,$campaingDefaults,$notifyOnSubscribe=false,$notifyOnUnsubscribe,$emailTypeOption,
+                        $visibility='pub')
+    {
+        $_params= array('name'=>$name,'contact'=>$contact,'permission_remainder'=>$permissionRemanider,'use_archive_bar'=>$useArchiveBar,
+            'campaignDefaults'=>$campaingDefaults,'notify_on_subscribe'=>$notifyOnSubscribe,'notify_on_unsubscribe'=>$notifyOnUnsubscribe,
+            'email_type_option'=>$emailTypeOption,'visibility'=>$visibility);
+        return $this->master->call('lists',$_params,Mailchimp::POST);
+    }
+    public function getLists($id=null,$fields=null,$excludeFields=null,$count=null,$offset=null,$beforeDateCreated=null,$sinceDateCreated=null,
+                                $beforeCampaignLastSent=null,$sinceCampaignLastSent=null,$email=null)
+    {
+        $_params = array();
+        if($fields)
+        {
+            $_params['fields'] = $fields;
+        }
+        if($excludeFields)
+        {
+            $_params['exclude_fields'] = $excludeFields;
+        }
+        if($count)
+        {
+            $_params['count'] = $count;
+        }
+        if($offset)
+        {
+            $_params['offset'] = $offset;
+        }
+        if($beforeDateCreated)
+        {
+            $_params['before_date_created'] = $beforeDateCreated;
+        }
+        if($sinceDateCreated)
+        {
+            $_params['since_date_created'] = $sinceDateCreated;
+        }
+        if($beforeCampaignLastSent)
+        {
+            $_params['before_campaigns_last_sent'] = $beforeCampaignLastSent;
+        }
+        if($sinceCampaignLastSent)
+        {
+            $_params['since_campaign_last_sent'] = $sinceCampaignLastSent;
+        }
+        if($email)
+        {
+            $_params['email'] = $email;
+        }
+        if($id) {
+            return $this->master->call('lists/'.$id, $_params, Mailchimp::GET);
+        }
+        else
+        {
+            return $this->master->call('lists', $_params, Mailchimp::GET);
+        }
+    }
+
+}
