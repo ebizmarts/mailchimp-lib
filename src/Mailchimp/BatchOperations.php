a64cc9e45834086fbc85f24162f2e788f45bc304
gonzalo <gonzalo@ebizmarts.com>
2016-05-03 15:32:40 -0300

    batchs implementation

--- src/Mailchimp/BatchOperations.php
+++ src/Mailchimp/BatchOperations.php
@@ -12,4 +12,49 @@
  */
 class Mailchimp_BatchOperations extends Mailchimp_Abstract
 {
+    /**
+     * @param $operations       An array of objects that describes operations to perform.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     */
+    public function add($operations)
+    {
+        return $this->master->call('batches',$operations,Mailchimp::POST);
+    }
+
+    /**
+     * @param $id               The unique id for the batch operation.
+     * @param $fields           A comma-separated list of fields to return. Reference parameters of sub-objects with dot notation.
+     * @param $excludeFields    A comma-separated list of fields to exclude. Reference parameters of sub-objects with dot notation.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     */
+    public function status($id,$fields=null,$excludeFields=null)
+    {
+        $_params = array();
+        if($fields) $_params['fields'] = $fields;
+        if($excludeFields) $_params['exclude_fields'] = $excludeFields;
+        return $this->master->call('batches/'.$id,$_params,Mailchimp::GET);
+    }
+
+    /**
+     * @param $fields           A comma-separated list of fields to return. Reference parameters of sub-objects with dot notation.
+     * @param $excludeFields    A comma-separated list of fields to exclude. Reference parameters of sub-objects with dot notation.
+     * @param $count            The number of records to return.
+     * @param $offset           The number of records from a collection to skip. Iterating over large collections with this parameter can be slow.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     */
+    public function getAll($fields=null,$excludeFields=null,$count=null,$offset=null)
+    {
+        $_params = array();
+        if($fields) $_params['fields'] = $fields;
+        if($excludeFields) $_params['exclude_fields'] = $excludeFields;
+        if($count) $_params['count'] = $count;
+        if($offset) $_params['offset'] = $offset;
+        return $this->master->call('batches',$_params,Mailchimp::GET);
+    }
 }
