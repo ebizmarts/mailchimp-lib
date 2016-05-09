5312387ab70244050bcf8f02fc14fc16bfc6facf
gonzalo <gonzalo@ebizmarts.com>
2016-05-09 16:11:09 -0300

    campaigns

--- src/Mailchimp/CampaignsFeedback.php
+++ src/Mailchimp/CampaignsFeedback.php
@@ -12,5 +12,83 @@
  */
 class Mailchimp_CampaignsFeedback extends Mailchimp_Abstract
 {
+    /**
+     * @param $campaignId           The unique id for the campaign.
+     * @param null $blockId         The block id for the editable block that the feedback addresses.
+     * @param $message              The content of the feedback.
+     * @param null $isComplete      The status of feedback.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     */
+    public function add($campaignId,$blockId=null,$message,$isComplete=null)
+    {
+        $_params=array('message'=>$message);
+        if($blockId) $_params['block_id'] = $blockId;
+        if($isComplete) $_params['is_complete'] = $isComplete;
+        return $this->master->call('campaigns/'.$campaignId.'/feedback',$_params,Mailchimp::POST);
+    }
 
+    /**
+     * @param $campaignId           The unique id for the campaign.
+     * @param null $fields          A comma-separated list of fields to return. Reference parameters of sub-objects with dot notation.
+     * @param null $excludeFields   A comma-separated list of fields to exclude. Reference parameters of sub-objects with dot notation.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     */
+    public function getAll($campaignId,$fields=null,$excludeFields=null)
+    {
+        $_params=array();
+        if($fields) $_params['fields'] = $fields;
+        if($excludeFields) $_params['exclude_fields'] = $excludeFields;
+        return $this->master->call('campaigns/'.$campaignId.'/feedback',$_params,Mailchimp::GET);
+    }
+
+    /**
+     * @param $campaignId               The unique id for the campaign.
+     * @param $feedbackId               The unique id for the feedback message.
+     * @param null $fields              A comma-separated list of fields to return. Reference parameters of sub-objects with dot notation.
+     * @param null $excludeFields       A comma-separated list of fields to exclude. Reference parameters of sub-objects with dot notation.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     */
+    public function get($campaignId,$feedbackId,$fields=null,$excludeFields=null)
+    {
+        $_params=array();
+        if($fields) $_params['fields'] = $fields;
+        if($excludeFields) $_params['exclude_fields'] = $excludeFields;
+        return $this->master->call('campaigns/'.$campaignId.'/feedback/'.$feedbackId,$_params,Mailchimp::GET);
+    }
+
+    /**
+     * @param $campaignId       The unique id for the campaign.
+     * @param $feedbackId       The unique id for the feedback message.
+     * @param null $blockId     The block id for the editable block that the feedback addresses.
+     * @param $message          The content of the feedback.
+     * @param $isComplete       The status of feedback.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     */
+    public function modify($campaignId,$feedbackId,$blockId=null,$message,$isComplete)
+    {
+        $_params=array('message'=>$message);
+        if($blockId) $_params['block_id'] = $blockId;
+        if($isComplete) $_params['is_complete'] = $isComplete;
+        return $this->master->call('campaigns/'.$campaignId.'/feedback/'.$feedbackId,$_params,Mailchimp::PATCH);
+    }
+
+    /**
+     * @param $campaignId       The unique id for the campaign.
+     * @param $feedbackId       The unique id for the feedback message.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     */
+    public function delete($campaignId,$feedbackId)
+    {
+        return $this->master->call('campaigns/'.$campaignId.'/feedback/'.$feedbackId,null,Mailchimp::DELETE);
+    }
 }
--- src/Mailchimp/CampaignsSendChecklist.php
+++ src/Mailchimp/CampaignsSendChecklist.php
@@ -12,5 +12,21 @@
  */
 class Mailchimp_CampaignsSendChecklist extends Mailchimp_Abstract
 {
+    /**
+     * @param $campaignId               The unique id for the campaign.
+     * @param null $fields              A comma-separated list of fields to return. Reference parameters of sub-objects with dot notation.
+     * @param null $excludeFields       A comma-separated list of fields to exclude. Reference parameters of sub-objects with dot notation.
+     * @return mixed
+     * @throws Mailchimp_Error
+     * @throws Mailchimp_HttpError
+     * @throws Mailchimp_HttpError
+     */
+    public function get($campaignId,$fields=null,$excludeFields=null)
+    {
+        $_params=array();
+        if($fields) $_params['fields'] = $fields;
+        if($excludeFields) $_params['exclude_fields'] = $excludeFields;
+        return $this->master->call('campaigns/'.$campaignId.'/send-checklist',$_params,Mailchimp::GET);
+    }
 
 }
