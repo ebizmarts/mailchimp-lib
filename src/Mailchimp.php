d6c0925467d24089d57c839368b1ebf45a748afe
Wojciech Naruniec <wojtek@rocketweb.com>
2018-03-14 09:03:03 +0100

    Reduce CURL timeout to 60 seconds

--- src/Mailchimp.php
+++ src/Mailchimp.php
@@ -103,7 +103,7 @@ class Mailchimp
         curl_setopt($this->_ch, CURLOPT_HEADER, false);
         curl_setopt($this->_ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->_ch, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($this->_ch, CURLOPT_TIMEOUT, 600);
+        curl_setopt($this->_ch, CURLOPT_TIMEOUT, 60);
 
         $this->root                                         = new Mailchimp_Root($this);
         $this->authorizedApps                               = new Mailchimp_AuthorizedApps($this);
