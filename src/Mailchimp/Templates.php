ebe95bb4612563b6f328ca3767bb60ed8dca5464
Maikel Koek <maikel@h-o.nl>
2018-11-09 11:59:00 +0100

    Implement retrieving list of templates

--- src/Mailchimp/Templates.php
+++ src/Mailchimp/Templates.php
@@ -16,4 +16,29 @@ class Mailchimp_Templates extends Mailchimp_Abstract
      * @var Mailchimp_TemplatesDefaultContent
      */
     public $defaultContent;
+
+    /**
+     * @param int $id                   The store id.
+     * @param string[] $fields          A comma-separated list of fields to return. Reference parameters of sub-objects with dot notation.
+     * @param string[] $excludeFields   A comma-separated list of fields to exclude. Reference parameters of sub-objects with dot notation.
+     * @param int $count                The number of records to return.
+     * @param int $offset               The number of records from a collection to skip. Iterating over large collections with this parameter can be slow.
+     * @return mixed
+     * @throws \Mailchimp_Error
+     * @throws \Mailchimp_HttpError
+     */
+    public function get($id=null,$fields=null,$excludeFields=null,$count=null,$offset=null)
+    {
+        $_params=array();
+        if($fields) $_params['fields'] = $fields;
+        if($excludeFields) $_params['exclude_fields'] = $excludeFields;
+        if($count) $_params['count'] = $count;
+        if($offset) $_params['offset'] = $offset;
+        if($id) {
+            return $this->master->call('templates/'.$id, $_params, \Mailchimp::GET);
+        }
+        else {
+            return $this->master->call('templates', $_params, \Mailchimp::GET);
+        }
+    }
 }
